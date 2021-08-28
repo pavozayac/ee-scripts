6f5864738b03ffd4dbc6560a6f6050ea954d7b33
Pablo Z <exaus.lol@gmail.com>
2021-08-28 17:22:52 +0200

    Added encoding to convolutional codes

--- convolutional.py
+++ convolutional.py
@@ -1,16 +1,22 @@
 from typing import Callable, List
+from itertools import product
+import numpy as np
 
-def push_shift(num: int, arr: list):
-    for i in range(len(arr)-1, 0, -1):
-        arr[i] = arr[i-1]
-    arr[0] = num
+def chunks(lst: list, n: int):
+       for i in range(0, len(lst), n):
+              yield lst[i:i + n]
+
+
+def push_shift(next: bool, arr: List[bool]):
+    arr.pop[0]
+    arr.append(next)
 
 class ConvCode:
     trellis: dict
     register_size: int
     output_size: int
-    register: list
-    gates: Callable[[List[int]], List[int]]
+    register: List[bool]
+    gates: Callable[[List[bool]], List[bool]]
 
     
     def __init__(self, gates, reg_size = 3, out_size = 2, ):
@@ -18,47 +24,57 @@ class ConvCode:
         self.reg_size = reg_size
         self.out_size = out_size
         self.gates = gates
-        self.register = [0]*reg_size
+        self.register = [False]*reg_size
 
-    def push_reg(self, bit: int):
+        self.fill_trellis()
+
+    def push_reg(self, bit: bool):
         output = self.gates(self.register)
-        self.register.pop(0)
-        self.register.append(bit)
+        self.register = self.register[1:] + [bit]
 
         return output
 
-    def generate_trellis(self):
+    def fill_trellis(self):
+        posregs = product([False, True], repeat=self.reg_size)
 
-        for i in range(2):
-            for j in range(2):
-                
+        for reg in posregs:
+            self.trellis[reg] = {}
+            self.register = list(reg).copy()
 
-                self.trellis[[i, j]] = {
+            for next_bit in [False, True]:
+                output = self.push_reg(next_bit)
 
+                self.trellis[reg][next_bit] = {
+                    'next_state': tuple(self.register),
+                    'output': output
                 }
 
-    def encode(self, bits) -> list:
 
-        encoded_seq = []
+    def encode(self, bits: List[int]) -> List[int]:
+        boolean_bits = map(lambda x: bool(x), bits)
+        output: List[int] = []
 
-        registers = [0, 0]
+        for bit in boolean_bits:
+            output += map(lambda x: int(x), self.push_reg(bit))
 
-        for bit in bits:
-            o1 = (bit + registers[1]) % 2
-            o2 = (bit + registers[0] + registers[1]) % 2
+        return output
 
-            encoded_seq.append(o1)
-            encoded_seq.append(o2)
+    def decode(self, bits: List[int]):
+        boolean_bits = list(map(lambda x: bool(x), bits))
+        grouped_bits = chunks(boolean_bits, self.out_size)
 
-            push_shift(bit, registers)
 
-        return encoded_seq
 
+
+
+
+
+def half_gates(input: List[bool]):
+    return [input[0] ^ input[1] ^ input[2], input[1]]
 
 if __name__ == '__main__':
-    
-    b = ConvCode()
+    b = ConvCode(half_gates, reg_size=3, out_size=2)
 
-    msg = [1,1,0]
+    test = np.random.randint(0, 2, 10000 * 100, int)
 
-    print(b.encode(msg))
+    print(len(b.encode(test)))
